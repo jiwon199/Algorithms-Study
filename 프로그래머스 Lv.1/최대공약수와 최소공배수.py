88bd62f8fa73c8105acb05f8b4e4276980140bcc
jiwon199 <53902714+jiwon199@users.noreply.github.com>
2022-01-24 19:03:01 +0900

    Create 최대공약수와 최소공배수.py

---
+++ 프로그래머스 Lv.1/최대공약수와 최소공배수.py
@@ -0,0 +1,13 @@
+def solution(n, m):
+    answer = []
+    if n>m:
+        n,m=m,n
+    for i in range(m,0,-1):
+        if n%i==0 and m%i==0:
+            answer.append(i)
+            break
+    for i in range(1,n*m+1):
+        if i%n==0 and i%m==0:
+            answer.append(i)
+            break
+    return answer
