32e6543e249e24dd86cde8b30d26b29491787ab8
jiwon199 <53902714+jiwon199@users.noreply.github.com>
2022-03-10 23:28:32 +0900

    Update 암호만들기.py

--- DP/암호만들기.py
+++ DP/암호만들기.py
@@ -6,19 +6,19 @@ if (code[0] == '0'):
   flag=True 
 dp[0] = dp[1] = 1
 	
-for i in range(2, n+1) :
-	cur = i - 1; 
-	if code[cur] == '0' and (code[cur - 1] < '1' or code[cur - 1] > '2'):  
+for i in range(2,n+1):
+  cur=i-1
+  if code[cur]=='0' and (code[cur-1]<'1' or code[cur-1]>'2'):
     flag=True
   if code[cur]!='0':
     dp[i]+=dp[i-1]
-	if (code[cur - 1] == '1' or (code[cur] <= '6' and code[cur - 1] == '2')) : 
-			dp[i] += dp[i - 2]  			
-	dp[i] %= 1000000
+  if code[cur-1]=='1' or (code[cur-1]=='2' and code[cur]<='6'):
+    dp[i]+=dp[i-2]
+  
 answer = dp[n]
 if flag==True:
   print(0)
 else:
-  print(answer)
+  print(answer%1000000)
 
 	 
