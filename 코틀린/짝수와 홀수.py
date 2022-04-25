c783f20d57050a0db0dfcd3e07d95d60ce37da47
jiwon199 <53902714+jiwon199@users.noreply.github.com>
2022-04-25 19:59:55 +0900

    Create 짝수와 홀수.py

---
+++ 코틀린/짝수와 홀수.py
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(num: Int): String {
+         
+        if (num%2==0){
+            return "Even"
+        }
+        else{
+            return "Odd"
+        }
+        
+    }
+}
